7bad1e0984e6f3379da261724cd91a610dd10c10
soynog <nicolas.o.garcia@gmail.com>
2016-06-05 20:04:45 -0400

    Add inverse_of and dependent_destroy
    - To both users and trips, in relationship to invitations (and trips, on users)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@
 class User < ActiveRecord::Base
   include Authentication
   has_many :examples
-  has_many :invitations
+  has_many :invitations, inverse_of: :user, dependent: :destroy
   has_many :trips, through: :invitations
 end
