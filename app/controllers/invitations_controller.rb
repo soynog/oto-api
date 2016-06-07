8e42f3adf62986f61d786ef203577d1d2d62bbf8
soynog <nicolas.o.garcia@gmail.com>
2016-06-07 11:20:33 -0400

    Test Invitations CRUD
    - Can create invitations by passing user and trip ID

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,4 @@
-class InvitationsController < ApplicationController
+class InvitationsController < ProtectedController
   before_action :set_invitation, only: [:show, :update, :destroy]
 
   # GET /invitations
@@ -18,6 +18,11 @@ class InvitationsController < ApplicationController
   # POST /invitations
   # POST /invitations.json
   def create
+    # puts "Current User is: #{current_user[:id]}"
+    # print "Params: #{invitation_params}"
+    # invitation_params[:invitation][:user_id] = 1 if invitation_params[:invitation][:user_id] == nil
+    # print "Params: #{invitation_params}"
+
     @invitation = Invitation.new(invitation_params)
 
     if @invitation.save
